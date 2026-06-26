c948252e4e827acc7641e0ffa2251fd0d081bf10
Administrator <liu3024141812@gmail.com>
2026-06-26 09:09:29 +0800

    第三次提交

--- src/main/java/com/example/stud/controller/StudClassController.java
+++ src/main/java/com/example/stud/controller/StudClassController.java
@@ -6,10 +6,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -29,13 +35,37 @@ public class StudClassController {
     @FXML
     private TableView<StudClass> results;
 
-
-    private final StudClassDao studClassDao = new StudClassDao();
-
+    @FXML
+    private Button searchbutton;
 
     @FXML
-    void insert(ActionEvent event) {
-        System.out.println("insert clicked");
+    private Button insertbutton;
+
+    @FXML
+    private Button deletebutton;
+
+
+    private  StudClassDao studClassDao = new StudClassDao();
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void insert(ActionEvent event) throws IOException {
+        System.out.println("new window");
+        //fxml文件从硬盘加载到内存 成为一幅画
+       FXMLLoader loader= new FXMLLoader(this.getClass().getResource("/com/example/stud/stud-class-insert.fxml"));
+//内存中的xml根节点
+        Parent root= loader.load();
+        //stage是fx的窗口window  开启一个新窗口
+        Stage stage = new Stage();
+        //scene相当于相框，将上述fxml文件加载到窗口
+        stage.setScene(new Scene(root));
+        //展示
+        stage.showAndWait();
+
     }
 
     @FXML
